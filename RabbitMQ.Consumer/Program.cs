603120eb34b603b2ce3fc6dd5bf8daa654737c4f
KORUCU <yusuf.korucu@bilgeadam.com>
2023-05-24 11:21:18 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Connection
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://utmrfjnx:5EAFe_eJPnPfK04-bk1qfDp6YVP51MbT@chimpanzee.rmq.cloudamqp.com/utmrfjnx");
+
+//Connection active Channeş Open
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channnel = connection.CreateModel();
+
+//Quee oluşurma
+channnel.QueueDeclare(queue: "korucu-test-quee", exclusive: false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channnel);
+
+channnel.BasicConsume(queue: "korucu-test-quee", autoAck: false, consumer: consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    //response message operation
+    //e.Body:quue message data
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+}
+Console.Read();
